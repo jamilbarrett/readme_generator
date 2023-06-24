754a1bd36a7883bc037c50fe67447742232397bc
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-06-24 00:03:04 -0400

    fixed readme issue

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 
-    # Title: t
-    ## Description: t
-    ## Installation: t
-    ## Usage: t
-    ## License: 
-    IT License, Apache License 2.0, GNU General Public License (GPL), BSD 2-Clause License, BSD 3-Clause License, ISC License, Mozilla Public License 2.0, Creative Commons licenses (various versions), GNU Lesser General Public License (LGPL), The Unlicense, WTFPL (Do What The F*ck You Want To Public License), Beerware License, zlib License, Dojo Toolkit License, Eclipse Public License, Microsoft Public License (Ms-PL), PostgreSQL License, SQLite License, Open Software License (OSL), Academic Free License (AFL), Artistic License 2.0
-    ## Contributors: t
-    ## Test: t
-    
+# Title: t
+## Description: t
+## Installation: t
+## Usage: t
+## License: 
+- GNU General Public License (GPL)
+## Contributors: t
+## Test: t
--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const fs = require('fs')
 
 
 // TODO: Create an array of questions for user input
-// const questions = [
-inquirer.prompt([
+const questions = [
     {
         name: 'title',
         message: 'What is the title of your ReadMe?'
@@ -28,7 +27,29 @@ inquirer.prompt([
         name: 'license',
         type: 'checkbox',
         message: 'Please select your license',
-        choices: ["IT License, Apache License 2.0, GNU General Public License (GPL), BSD 2-Clause License, BSD 3-Clause License, ISC License, Mozilla Public License 2.0, Creative Commons licenses (various versions), GNU Lesser General Public License (LGPL), The Unlicense, WTFPL (Do What The F*ck You Want To Public License), Beerware License, zlib License, Dojo Toolkit License, Eclipse Public License, Microsoft Public License (Ms-PL), PostgreSQL License, SQLite License, Open Software License (OSL), Academic Free License (AFL), Artistic License 2.0"]
+        choices: [
+            'MIT License',
+            'Apache License 2.0',
+            'GNU General Public License (GPL)',
+            'BSD 2-Clause License',
+            'BSD 3-Clause License',
+            'ISC License',
+            'Mozilla Public License 2.0',
+            'Creative Commons licenses (various versions)',
+            'GNU Lesser General Public License (LGPL)',
+            'The Unlicense',
+            'WTFPL (Do What The F*ck You Want To Public License)',
+            'Beerware License',
+            'zlib License',
+            'Dojo Toolkit License',
+            'Eclipse Public License',
+            'Microsoft Public License (Ms-PL)',
+            'PostgreSQL License',
+            'SQLite License',
+            'Open Software License (OSL)',
+            'Academic Free License (AFL)',
+            'Artistic License 2.0'
+        ]
     },
     {
         name: 'contributors',
@@ -42,23 +63,24 @@ inquirer.prompt([
         name: 'questions',
         message: 'How can one reach out for support or with general questions?'
     }
-])
-.then(({title, description, install, usage, license, contributors, test}) => {
-    const content = `
-    # Title: ${title}
-    ## Description: ${description}
-    ## Installation: ${install}
-    ## Usage: ${usage}
-    ## License: 
-    ${license.map(lic => `${lic}`).join('\n')}
-    ## Contributors: ${contributors}
-    ## Test: ${test}
-    `
-  
-    console.log('README.md file has been created successfully.') 
-    fs.writeFile('./README.md',content, err => {  if (err) throw err
+]
+inquirer.prompt(questions)
+    .then(({ title, description, install, usage, license, contributors, test }) => {
+        const content = `
+# Title: ${title}
+## Description: ${description}
+## Installation: ${install}
+## Usage: ${usage}
+## License: 
+${license.map(lic => `- ${lic}`).join('\n')}
+## Contributors: ${contributors}
+## Test: ${test}
+`
+        console.log('README.md file has been created successfully.')
+        fs.writeFile('./README.md', content, err => {
+            if (err) throw err
+        })
     })
-})
 // TODO: Create a function to write README file
 
 // TODO: Create a function to initialize app
